ec87e566778690fbe4856dd249bade3db0c2f020
Belal Mohamed <belalmohamedsaad111@gmail.com>
2026-09-08 17:56:11 +0300

    Change model version from gemini-2.0 to gemini-1.5

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def reshape_text(text):
 
 def generate_chapter(prompt):
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-1.5-flash",
         contents=prompt,
     )
     return response.text
